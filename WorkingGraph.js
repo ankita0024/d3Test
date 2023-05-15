7615663b48b6b51682ce2d6afce9b31b1e86c68d
ankita0024 <ankita.ankita024@gmail.com>
2023-05-15 01:49:14 -0500

    Update WorkingGraph.js

--- WorkingGraph.js
+++ WorkingGraph.js
@@ -1,220 +1,362 @@
-let h = window.innerHeight;
-let w = window.innerWidth;
+// let h = window.innerHeight;
+// let w = window.innerWidth;
 
-let time = 0;
-let num = 100;
 var margin = { top: 10, right: 10, bottom: 100, left: 40 },
   margin2 = { top: 430, right: 10, bottom: 20, left: 40 },
   width = 960 - margin.left - margin.right,
   height = 500 - margin.top - margin.bottom,
   height2 = 500 - margin2.top - margin2.bottom;
+let sources = [];
 
-let data = [0];
-let latestData = [0];
-let latestAverages_25 = [0];
-let latestAverages_50 = [0];
-let averages_25 = [0];
-let averages_50 = [0];
+var myBrush;
+var color = d3.scale.category10();
+var parseDate = d3.time.format("%Y-%m-%d %H:%M:%S");
 
-
-let svg = d3.select('body').append('svg')
-  .attr({width:  width + margin.left + margin.right, height: height + margin.top + margin.bottom});
-
-let x = d3.scale.linear().range([0, w - 40]);
+let svg = d3
+  .select("body")
+  .append("svg")
+  .attr({
+    width: width + margin.left + margin.right,
+    height: height + margin.top + margin.bottom,
+  });
+svg
+  .append("defs")
+  .append("clipPath")
+  .attr("id", "clip")
+  .append("rect")
+  .attr("width", width)
+  .attr("height", height);
+let x = d3.time.scale().range([0, width]);
 let y = d3.scale.linear().range([height, 0]);
 
-let x2 = d3.scale.linear().range([0, w - 40]);
+let x2 = d3.time.scale().range([0, width]);
 let y2 = d3.scale.linear().range([height2, 0]);
 
-let line = d3.svg.line()
-  .x((d, i) => x(i + time - num))
-  .y(d => y(d));
+let line = d3.svg
+  .line()
+  .x(function (d) {
+    return x(d.date);
+  })
+  .y(function (d) {
+    return y(d.value);
+  })
+  .defined(function (d) {
+    return !isNaN(d.value);
+  })
+  .interpolate("linear");
 
-let line2 = d3.svg.line()
-.x((d, i) => x2(i))
-.y(d => y2(d));
+let line2 = d3.svg
+  .line()
+  .x(function (d) {
+    return x2(d.date);
+  })
+  .y(function (d) {
+    return y2(d.value);
+  })
+  .defined(function (d) {
+    return !isNaN(d.value);
+  })
+  .interpolate("linear");
 
-let xAxis = d3.svg.axis()
-  .scale(x)
-  .orient('bottom');
+let main = svg
+  .append("g")
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+let graph2 = svg
+  .append("g")
+  .attr("transform", "translate(" + margin2.left + "," + margin2.top + ")");
 
-let yAxis = d3.svg.axis()
-  .scale(y)
-  .orient('left');
+// graph2.append('g')
+//   .attr('class', 'x axis2')
+//   .attr('transform', "translate(0," + height2 + ")")
+//   .call(d3.svg.axis()
+//         .scale(x2)
+//         .orient('bottom'));
 
-let main = svg.append("g").attr('transform', "translate(" + margin.left + "," + margin.top + ")");
-let graph2 = svg.append('g').attr('transform', "translate(" + margin2.left + "," + margin2.top + ")");
+// let $data = main.append('path')
+//   .attr('class', 'line data');
 
-let $xAxis = main.append('g')
-  .attr('class', 'x axis')
-  .attr('transform', "translate(0," + height + ")")
-  .call(xAxis);
+// let $averages_25 = main.append('path')
+//   .attr('class', 'line average-25');
 
-let $yAxis = main.append('g')
-  .attr('class', 'y axis')
-  .call(yAxis);
+// let $averages_50 = main.append('path')
+//   .attr('class', 'line average-50');
 
-graph2.append('g')
-  .attr('class', 'x axis2')
-  .attr('transform', "translate(0," + height2 + ")")
-  .call(d3.svg.axis()
-        .scale(x2)
-        .orient('bottom'));
+// let $tooltip = d3.select("body").append("div")
+//   .attr("class", "tooltip")
+//   .style("opacity", 0);
 
-let $data = main.append('path')
-  .attr('class', 'line data');
+// let $data2 = graph2.append('path').attr('class', 'line');
 
-let $averages_25 = main.append('path')
-  .attr('class', 'line average-25');
+// let legend = main.append('g')
+//   .attr('transform', `translate(20, 30)`)
+//   .selectAll('g')
+//   .data([['X Axis', '#fff'], ['Y Axis', '#0ff'], ['Z Axis', '#ff0']])
+//   .enter()
+//   .append('g');
 
-let $averages_50 = main.append('path')
-  .attr('class', 'line average-50');
+//   legend
+//     .append('circle')
+//     .attr('fill', d => d[1])
+//     .attr('r', 5)
+//     .attr('cx', 0)
+//     .attr('cy', (d, i) => i * 15);
 
-let $tooltip = d3.select("body").append("div")
-  .attr("class", "tooltip")
-  .style("opacity", 0);
-
-let $data2 = graph2.append('path').attr('class', 'line');
-
-let legend = main.append('g')
-  .attr('transform', `translate(20, 30)`)
-  .selectAll('g')
-  .data([['X Axis', '#fff'], ['Y Axis', '#0ff'], ['Z Axis', '#ff0']])
-  .enter()
-  .append('g');
-
-  legend
-    .append('circle')
-    .attr('fill', d => d[1])
-    .attr('r', 5)
-    .attr('cx', 0)
-    .attr('cy', (d, i) => i * 15);
-
-  legend
-    .append('text')
-    .text(d => d[0])
-    .attr('transform', (d, i) => `translate(10, ${i * 15 + 4})`);
+//   legend
+//     .append('text')
+//     .text(d => d[0])
+//     .attr('transform', (d, i) => `translate(10, ${i * 15 + 4})`);
 
 function tick() {
-  time++;
-  data = rls.x;
-  averages_25 = rls.y;
-  averages_50 = rls.z;
-
-  if (time > num) {
-    latestData = data.slice(-num);
-    latestAverages_25 = averages_25.slice(-num);
-    latestAverages_50 = averages_50.slice(-num);
-  }
-}
-
-function update() {
-  x.domain([time - num, time]);
-  let yDom = d3.extent(latestData);
-  yDom[0] = Math.max(yDom[0] - 1, 0);
-  yDom[1] += 1;
-  y.domain(yDom);
-
+  color.domain(
+    d3.keys(rls[0]).filter(function (key) {
+      return key !== "date";
+    })
+  );
+  rls.forEach(function (d) {
+    d.date = new Date(d.date);
+  });
+  sources = color.domain().map(function (name) {
+    return {
+      name: name,
+      values: rls.map(function (d) {
+        return { date: d.date, value: +d[name] };
+      }),
+    };
+  });
+  x.domain(
+    d3.extent(rls, function (d) {
+      return d.date;
+    })
+  );
+  y.domain([
+    d3.min(sources, function (c) {
+      return d3.min(c.values, function (v) {
+        return v.value;
+      });
+    }),
+    d3.max(sources, function (c) {
+      return d3.max(c.values, function (v) {
+        return v.value;
+      });
+    }),
+  ]);
   x2.domain(x.domain());
   y2.domain(y.domain());
 
-  $xAxis
-    .call(xAxis);
+  var mainLineGroups = main.selectAll("g").data(sources).enter().append("g");
 
-  $yAxis
-    .call(yAxis);
+  mainLineGroups
+    .append("path")
+    .attr("class", "line")
+    .attr("d", function (d) {
+      return line(d.values);
+    })
+    .style("stroke", function (d) {
+      return color(d.name);
+    })
+    .attr("clip-path", "url(#clip-path)");
 
-  $data
-    .datum(latestData)
-    .attr('d', line);
-    // .on("mouseover", function(d) {
-    //   $tooltip.style("opacity", 1); // Make tooltip visible
-    // })
-    // .on("mouseout", function(d) {
-    //   $tooltip.style("opacity", 0); // Hide tooltip
-    // })
-    // .on('mousemove', function() {
-    //   let xPos = d3.mouse(this)[0];
-    //   let xIndex = Math.round(x.invert(xPos - 30)) - time + num;
-    //   let yValue = latestData[xIndex];
-    //   showTooltip(xPos, y(yValue), yValue);
-    // })
-    // .on('mouseout', hideTooltip);
+  main
+    .append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.svg.axis().scale(x).orient("bottom"));
 
-  $averages_25
-    .datum(latestAverages_25)
-    .attr('d', line);
+  main
+    .append("g")
+    .attr("class", "y axis")
+    .call(d3.svg.axis().scale(y).orient("left"));
 
-  $averages_50
-    .datum(latestAverages_50)
-    .attr('d', line);
+  var graphLineGroups = graph2.selectAll("g").data(sources).enter().append("g");
 
-    $data2.datum(latestData).attr('d', line);
+  graphLineGroups
+    .append("path")
+    .attr("class", "line")
+    .attr("d", function (d) {
+      return line2(d.values);
+    })
+    .style("stroke", function (d) {
+      return color(d.name);
+    })
+    .attr("clip-path", "url(#clip-path)");
+  
+  // graph2.select(".x.axis2").remove();
+
+  graph2
+    .append("g")
+    .attr("class", "x axis2")
+    .attr("transform", "translate(0," + height2 + ")")
+    .call(d3.svg.axis().scale(x2).orient("bottom"));
+
+  myBrush = d3.svg
+    .brush()
+    .x(d3.scale.linear().range([0, width]))
+    .on("brush", brushed);
+
+  var begin = x2(
+    new Date(x2.invert(x2.range()[1]).valueOf() - 1000 * 365 * 8 * 24 * 60 * 60)
+  );
+  var end = x2.range()[1];
+  console.log("begin",begin);
+  console.log("end-",end);
+
+  graph2
+    .append("g")
+    .attr("class", "x brush")
+    .call(myBrush)
+    .selectAll("rect")
+    .attr("y", 0)
+    .attr("height", height2)
+    .attr("x", begin)
+    .attr("width", end - begin);
+  refreshChart();
 }
 
-update();
+function refreshChart() {
+  // sources.forEach(function (s) {
+  //   var lastDate = new Date(
+  //     s.values[s.values.length - 1].date.setMonth(
+  //       s.values[s.values.length - 1].date.getMonth() + 1
+  //     )
+  //   );
+  //   var value = s.values.shift().value;
+  //   s.values.push({ date: lastDate, value: value });
+  // });
+
+  x2.domain([
+    d3.min(sources, function (c) {
+      return d3.min(c.values, function (v) {
+        return v.date;
+      });
+    }),
+    d3.max(sources, function (c) {
+      return d3.max(c.values, function (v) {
+        return v.date;
+      });
+    }),
+  ]);
+
+  y2.domain([
+    d3.min(sources, function (c) {
+      return d3.min(c.values, function (v) {
+        return v.value;
+      });
+    }),
+    d3.max(sources, function (c) {
+      return d3.max(c.values, function (v) {
+        return v.value;
+      });
+    }),
+  ]);
+
+  // var bs = myBrush.brushSelection();
+  // if (bs) {
+  //   x.domain([x2.invert(bs[0]), x2.invert(bs[1])]);
+  // } else {
+  //   x.domain(x2.domain());
+  // }
+  // y.domain(y2.domain());
+
+  var updateMainData = main.selectAll("path.line").data(sources);
+  updateMainData
+    .enter()
+    .append("path")
+    .attr("class", "line")
+    .style("stroke", function (d) {
+      return color(d.name);
+    })
+    .attr("clip-path", "url(#clip)")
+    .attr("d", function (d) {
+      return line(d.values);
+    });
+  updateMainData
+    .transition()
+    .duration(500)
+    .attr("d", function(d) {
+      return line(d.values);
+    })
+    .style("stroke", function(d) {
+      return color(d.name);
+    });
+
+  updateMainData.exit().remove();
+  console.log("sources---", sources);
+
+  var updateGraph2Data = graph2.selectAll("path.line").data(sources);
+  updateGraph2Data
+    .enter()
+    .append("path")
+    .attr("class", "line")
+    .style("stroke", function (d) {
+      return color(d.name);
+    })
+    .attr("clip-path", "url(#clip)")
+    .attr("d", function (d) {
+      return line2(d.values);
+    });
+  updateGraph2Data
+    .transition() // animate any changes in data
+    .duration(500)
+    .attr("d", function(d) {
+      return line(d.values);
+    })
+    .style("stroke", function(d) {
+      return color(d.name);
+    });
+
+  updateGraph2Data.exit().remove();
+
+  main.select(".x.axis").call(d3.svg.axis().scale(x).orient("bottom"));
+  main.select(".y.axis").call(d3.svg.axis().scale(y).orient("left"));
+  graph2.select(".x.axis2").call(d3.svg.axis().scale(x2).orient("bottom"));
+};
 
 setInterval(() => {
-    if(recording && rls.x.length && rls.y.length && rls.z.length){
-        tick();
-        update();
-    }
-  
+  if (recording && rls.length) {
+    tick();
+  }
 }, 60);
 
-// Add brush
-let brushX = d3.svg.brush()
-  .x(x2)
-  .on('brush', brushed);
+// function brushed() {
+//   let brushExtent = brushX.extent();
+//   x.domain(brushExtent);
 
-graph2.append("g")
-  .attr("class", "brush")
-  .call(brushX)
-  .selectAll("rect")
-  .attr("y", 0)
-  .attr("height", height2 + 7);
+//   $data.attr("d", line);
+//   $averages_25.attr("d", line);
+//   $averages_50.attr("d", line);
 
-let $brushedData = svg.append('path')
-  .attr('class', 'brushed-data');
+//   x2.domain(brushExtent);
+//   let brushedData = latestData.filter((d, i) => {
+//     let xVal = i + time - num;
+//     return x2(xVal) >= brushExtent[0] && x2(xVal) <= brushExtent[1];
+//   });
+//   let yDom = d3.extent(brushedData);
+//   yDom[0] = Math.max(yDom[0] - 1, 0);
+//   yDom[1] += 1;
+//   y2.domain(yDom);
 
+//   $brushedData.datum(brushedData).attr("d", line2);
+
+//   $brush.call(brushX.extent([x.domain()[0], x.domain()[1]]));
+// }
 function brushed() {
-  let brushExtent = brushX.extent();
-  x.domain(brushExtent);
+  var s = myBrush.extent();
 
-  $data.attr('d', line);
-  $averages_25.attr('d', line);
-  $averages_50.attr('d', line);
+  x.domain(
+    myBrush.empty()
+      ? x2.domain()
+      : [x2.invert(s[0]), x2.invert(s[1])]
+  );
 
-  x2.domain(brushExtent);
-  let brushedData = latestData.filter((d, i) => {
-    let xVal = i + time - num;
-    return x2(xVal) >= brushExtent[0] && x2(xVal) <= brushExtent[1];
+  main.selectAll("path.line").attr("d", function(d) {
+    return line(d.values);
   });
-  let yDom = d3.extent(brushedData);
-  yDom[0] = Math.max(yDom[0] - 1, 0);
-  yDom[1] += 1;
-  y2.domain(yDom);
-
-  $brushedData.datum(brushedData)
-    .attr('d', line2);
-
-  $brush.call(brushX.extent([x.domain()[0], x.domain()[1]]));
+  main.select(".x.axis").call(d3.svg.axis().scale(x).orient("bottom"));
+  main.select(".y.axis").call(d3.svg.axis().scale(y).orient("left"));
 }
 
-function showTooltip(xPos, yPos, value) {
-  $tooltip
-    .html("Values: " + value)
-    .style('left', `${xPos}px`)
-    .style('top', `${yPos}px`)
-    .transition()
-    .duration(50)
-    .style('opacity', 1);
-}
-
-function hideTooltip() {
-  $tooltip
-    .transition()
-    .duration(50)
-    .style('opacity', 0);
-}
-
+Date.prototype.addMonths = function(months) {
+  var dat = new Date(this.valueOf());
+  dat.setMonth(dat.getMonth() + months);
+  return dat;
+};
